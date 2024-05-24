62fa0760e11ae95d9692ab2f3164924a8d099d1a
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2024-05-23 21:37:21 -0500

    uso de maps

--- lib/pages/trip/trip_controller.dart
+++ lib/pages/trip/trip_controller.dart
@@ -1,3 +1,28 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:location/location.dart';
 
-class TripController extends GetxController {}
+class TripController extends GetxController {
+  Location location = Location();
+  RxDouble latitude = (-11.99107547525432).obs;
+  RxDouble longitude = (-76.5996417595332).obs;
+  RxBool focused = false.obs;
+  final Key mapKey = UniqueKey();
+
+  Future<void> getLocation(MapController mapController) async {
+    try {
+      var currentLocationData = await location.getLocation();
+      print('getLocation');
+      print(currentLocationData.latitude);
+      print(currentLocationData.longitude);
+      latitude.value = currentLocationData.latitude!;
+      longitude.value = currentLocationData.longitude!;
+      mapController.move(LatLng(latitude.value, longitude.value), 15.0);
+      focused.value = true;
+    } catch (e) {
+      print('Error al obtener la ubicación: $e');
+    }
+  }
+}
--- lib/pages/trip/trip_page.dart
+++ lib/pages/trip/trip_page.dart
@@ -1,15 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import '../../configs/constants.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 import 'trip_controller.dart';
+import '../../configs/constants.dart';
 
 class TripPage extends StatelessWidget {
+  MapController mapController = MapController();
   TripController control = Get.put(TripController());
+  final Key mapKey = UniqueKey();
 
   Widget _buildBody(BuildContext context) {
+    control.getLocation(mapController);
     return SafeArea(
-      child: Text('Trip Page'),
-    );
+        child: Obx(() => Column(children: [
+              Expanded(
+                child: FlutterMap(
+                  key: mapKey,
+                  options: MapOptions(
+                    center:
+                        LatLng(control.latitude.value, control.longitude.value),
+                    minZoom: 5,
+                    maxZoom: 25,
+                    zoom: 5,
+                  ),
+                  children: [
+                    TileLayer(
+                      urlTemplate:
+                          'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                      userAgentPackageName: 'com.example.dr_gym',
+                    ),
+                    MarkerLayer(markers: [
+                      control.focused.value != false
+                          ? Marker(
+                              width: 40.0,
+                              height: 40.0,
+                              point: LatLng(control.latitude.value,
+                                  control.longitude.value),
+                              child: Container(
+                                child: Icon(
+                                  Icons.location_on,
+                                  size: 40.0,
+                                  color: appColor3,
+                                ),
+                              ),
+                            )
+                          : Marker(
+                              width: 40.0,
+                              height: 40.0,
+                              point: LatLng(0, 0),
+                              child: Container(
+                                child: Icon(
+                                  Icons.location_on,
+                                  size: 0.0,
+                                  color: appColor3,
+                                ),
+                              ),
+                            )
+                    ], key: mapKey)
+                  ],
+                  mapController: mapController,
+                ),
+              ),
+            ])));
   }
 
   @override
